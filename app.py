0e5ee8d9a42bdab8d94631bab0ba42f83a7447a7
Jorge Ejarque <jorge.ejarque@bsc.es>
2024-04-19 13:05:11 +0200

    Changes to execute AddPath

--- app.py
+++ app.py
@@ -1,41 +1,36 @@
 from flask import Flask, request, jsonify
 import subprocess
 import json
+import requests
 
 app = Flask(__name__)
 
-AVILABLE_SOFTWARE_JSON="config/available_software.json"
+#AVILABLE_SOFTWARE_JSON="config/available_software.json"
 
-def load_software():
-    with open(AVILABLE_SOFTWARE_JSON, 'r') as file:
-        return json.load(file)
+RHINO_LISTENER_URL = "http://localhost:4000"  # Replace PORT with the actual port number
+
+# def load_software():
+#     with open(AVILABLE_SOFTWARE_JSON, 'r') as file:
+#         return json.load(file)
 
 @app.route('/run', methods=['POST'])
 def run_command():
     try:
         # Get the command from the request
         data = request.get_json()
-        available_software = load_software()
-        software = data['software']
-        params = data['parameters']
-        command = available_software[software].format(**params)
-        # Run the command using subprocess
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = process.communicate()
-
-        # Check if the command executed successfully
-        if process.returncode == 0:
-            response = {'status': 'success', 'output': output.decode('utf-8')}
+        # Send a notification to the Rhino listener
+        response = requests.post(RHINO_LISTENER_URL, json=data)
+        
+        # Check response from Rhino
+        if response.status_code == 200:
+            return jsonify({'status': 'success', 'message': 'Notification sent to Rhino'}), 200
         else:
-            response = {'status': 'failure', 'output': error.decode('utf-8')}
-        return jsonify(response), 200
-    except KeyError as e:
-        message = f"'software' or 'parameters' not found: {str(e)}"
-        return jsonify({'status': 'error', 'message': message}), 500
-    
+            return jsonify({'status': 'error', 'message': 'Failed to notify Rhino'}), 500
+
     except Exception as e:
         message = f"Error: {type(e).__name__} - {str(e)}"
         return jsonify({'status': 'error', 'message': message}), 500
+
 
 
 if __name__ == '__main__':
